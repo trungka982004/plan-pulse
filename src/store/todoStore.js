b7071fe3a9957c012eb6a24d39484cab3d612315
TrungPT <125136900+trungka982004@users.noreply.github.com>
2026-03-13 09:46:12 +0700

    feat(store) init TodoStore using Composition API

--- src/store/todoStore.js
+++ src/store/todoStore.js
@@ -1,60 +1,77 @@
 import { defineStore } from "pinia"
+import { ref,computed  } from 'vue'
 
-export const useTodoStore = defineStore("todo", {
-  state: () => ({
-    todos: [],
-    filter: "all"
-  }),
-  
-  getters: {
-    filteredTodos: (state) => {
-      if (state.filter === "active") {
-        return state.todos.filter((t) => !t.done)
-      }
-      if (state.filter === "completed") {
-        return state.todos.filter((t) => t.done)
-      }
-      return state.todos
-    },
-    totalCount: (state) => state.todos.length,
-    completedCount: (state) => state.todos.filter((t) => t.done).length,
-    progress: (state) => {
-      if (state.todos.length === 0) return 0
-      return Math.round(
-        (state.todos.filter((t) => t.done).length / state.todos.length) * 100
-      )
-    }
-  },
+export const useTodoStore = defineStore("todo", () => {
+  // State
+  const todos = ref([])
+  const filter = ref('all')
 
-  actions: {
-    addTodo(text) {
-      this.todos.push({
+  //Getters
+  const filteredTodos = computed(() => {
+    if (filter.value === "active") return todos.value.filter((t) => !t.done)
+    if (filter.value === "completed") return todos.value.filter((t) => t.done)
+    return todos.value
+  })
+
+  const totalCount = computed(() => todos.value.length)
+  const completedCount = computed(() => todos.value.filter(t => t.done).length)
+
+  const progress = computed(() => {
+    if (totalCount.value === 0) return 0
+    return Math.round((completedCount.value / totalCount.value) * 100)
+  })
+
+  // Actions
+  const addTodo = (title) => {
+    if(title.trim()) {
+      todos.value.push({
         id: Date.now(),
-        text,
+        text: title,
         done: false
       })
-    }, 
-    editTodo(id, updateText) {
-      const todo = this.todos.find(t => t.id === id)
-      if(todo && updateText.trim()) {
-        todo.text = updateText
-      }
-    },
-    toggleTodo(id) {
-      const todo = this.todos.find(t => t.id === id)
-      if (todo) todo.done = !todo.done
-    }, 
-
-    removeTodo(id) { 
-      this.todos = this.todos.filter(t => t.id !== id)
-    },
-
-    clearAll() {
-      this.todos = []
-    },
-
-    setFilter(filter) {
-      this.filter = filter
     }
+  }
+
+  const editTodo = (id, updateText) => {
+    const todo = todos.value.find(t => t.id === id)
+    if(todo && updateText.trim()) {
+      todo.text = updateText
+    }
+  }
+
+  const toggleTodo = (id) => {
+    const todo = todos.value.find(t => t.id === id)
+    if (todo) todo.done = !todo.done
+  }
+
+  const removeTodo = (id) => {
+    todos.value = todos.value.filter(t => t.id !== id)
+  }
+
+  const clearAll = () => {
+    todos.value = []
+  }
+
+  const setFilter = (newFilter) => {
+    filter.value = newFilter
+  }
+
+  // Return component in reuse 
+  return {
+    //states
+    todos,
+    filter,
+    //getters
+    filteredTodos,
+    totalCount,
+    completedCount,
+    progress,
+    //actions
+    addTodo,
+    editTodo,
+    toggleTodo,
+    removeTodo,
+    clearAll,
+    setFilter
   }
 })
